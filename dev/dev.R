4f047d59dcd18a9351a6d5009e01ee2ad19d4064
Jurian <jurian@jurianbaas.nl>
2017-07-24 19:33:04 +0200

    Removed optional parameters

--- dev/dev.R
+++ dev/dev.R
@@ -3,14 +3,14 @@ library(subgroup.discovery)
 
 
 data(pima)
-p.cov <- prim.cover(class ~ ., data = pima, peeling.quantile = 0.05, min.support = 0.05, plot = TRUE)
+p.cov <- prim.cover(class ~ ., data = pima, plot = TRUE, optimal.box = "2se")
 summary(p.cov)
 plot(p.cov)
 
 
 
 data(pima)
-p.div <- prim.diversify(class ~ ., data = pima, n = 10, peeling.quantile = 0.05, min.support = 0.05, plot = TRUE)
+p.div <- prim.diversify(class ~ ., data = pima, n = 100, plot = TRUE, parallel = TRUE, optimal.box = "2se")
 summary(p.div)
 plot(p.div)
 
@@ -18,7 +18,7 @@ plot(p.div)
 
 
 data(ames)
-p.cov <- prim.cover(SalePrice ~ . - PID - Order, ames, peeling.quantile = 0.05, min.support = 0.1, plot = TRUE)
+p.cov <- prim.cover(SalePrice ~ . - PID - Order, ames, plot = TRUE)
 summary(p.cov)
 plot(p.cov)
 
@@ -26,7 +26,7 @@ plot(p.cov)
 
 
 data(ames)
-p.div <- prim.diversify(SalePrice ~ . - PID - Order, ames, n = 10, peeling.quantile = 0.05, min.support = 0.05, plot = TRUE)
+p.div <- prim.diversify(SalePrice ~ . - PID - Order, ames, n = 3, plot = TRUE)
 summary(p.div)
 plot(p.div)
 
