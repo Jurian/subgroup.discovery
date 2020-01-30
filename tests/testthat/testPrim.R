c1d7f0583130d653794a9acf03577fdf94456d3d
Jurian <j.baas@uu.nl>
2020-01-30 13:46:23 +0100

    Added tests for prim.box.index

--- tests/testthat/testPrim.R
+++ tests/testthat/testPrim.R
@@ -1,6 +1,7 @@
 library(subgroup.discovery)
 context("Patient Rule Induction Method")
 
+
 testthat::test_that("Test the data preparation phase", {
 
   data(credit)
@@ -14,26 +15,40 @@ testthat::test_that("Test the data preparation phase", {
 
 testthat::test_that("Test functionality on pima data set", {
 
+  set.seed(1234)
   data(pima)
   pima.sample <- sample(nrow(pima), 0.75*nrow(pima))
   pima <- prim.data.prepare(pima)
-  pima.model <- prim(class ~ ., data = pima[pima.sample,], peeling.quantile = 0.3, min.support = 0.4)
+  pima.model <- prim(class ~ ., data = pima[pima.sample,], peeling.quantile = 0.4, min.support = 0.4)
   pima.predict <- predict(pima.model, pima[-pima.sample,])
 
   expect_is(pima.model, "prim.peel")
   expect_is(pima.predict, "prim.predict")
 
+  pima.model.idx <- prim.box.index(pima.model, pima)
+  pima.predict.idx <- prim.box.index(pima.predict, pima)
+
+  expect_true(length(pima.model.idx) > 0)
+  expect_true(length(pima.predict.idx) > 0)
+  expect_true(length(pima.model.idx) <= length(pima.predict.idx))
 })
 
 testthat::test_that("Test functionality on ames data set, with lots of categorical data", {
 
+  set.seed(1234)
   data(ames)
   ames.sample <- sample(nrow(ames), 0.75*nrow(ames))
   ames <- prim.data.prepare(ames)
-  ames.model <- prim(SalePrice ~ . - PID - Order, data = ames[ames.sample,], peeling.quantile = 0.05, min.support = 0.1)
+  ames.model <- prim(SalePrice ~ . - PID - Order, data = ames[ames.sample,], peeling.quantile = 0.1, min.support = 0.1)
   ames.predict <- predict(ames.model, ames[-ames.sample,])
 
   expect_is(ames.model, "prim.peel")
   expect_is(ames.predict, "prim.predict")
 
+  ames.model.idx <- prim.box.index(ames.model, ames)
+  ames.predict.idx <- prim.box.index(ames.predict, ames)
+
+  expect_true(length(ames.model.idx) > 0)
+  expect_true(length(ames.predict.idx) > 0)
+  expect_true(length(ames.model.idx) <= length(ames.predict.idx))
 })
