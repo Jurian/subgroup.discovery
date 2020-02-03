cb60d51469f735d7916442449ab52aa5f666b09b
Jurian <j.baas@uu.nl>
2020-02-03 13:26:36 +0100

    Complying with best practices, added an unload() function to remove the dll

--- R/subgroup.discovery.R
+++ R/subgroup.discovery.R
@@ -1,3 +1,7 @@
+.onUnload <- function (libpath) {
+  library.dynam.unload("subgroup.discovery", libpath)
+}
+
 #' @useDynLib subgroup.discovery, .registration=TRUE
 #' @importFrom Rcpp evalCpp
 #' @importFrom RcppParallel RcppParallelLibs
